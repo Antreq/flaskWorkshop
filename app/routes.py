0c21c04d913c4faf7f05c7fd3c94f9226d9b4535
Antreq <a.antoniobravo@outlook.com>
2018-06-21 17:50:32 -0500

    Index

--- app/routes.py
+++ app/routes.py
@@ -7,12 +7,16 @@ def index():
 	user = {'username': 'Antonio'}
 	events = [
 		{
-			'name': 'Antonio', 
-			'date': '111111111111'
+			'name': 'Antreq', 
+			'date': '9167 3960 9126'
 		},
 		{
 			'name': 'Qiqe',
-			'date': '1111111111111'
+			'date': '1709 5990 0028'
+		}
+		{
+			'name': 'Saurios21',
+			'date': '1189 7617 0817'
 		}
 	]
 	return render_template('index.html', user=user, title='Testigos', events=events)
